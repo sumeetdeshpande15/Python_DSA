5768080154f82fc3d59a9af9d53325f0dc133f2e
Sumeet Deshpande <sumeet150621@gmail.com>
2023-07-11 09:33:16 +0530

    Add files via upload

---
+++ Big O/O(a * b).py
@@ -0,0 +1,33 @@
+def print_items(a, b):
+    for i in range(a):
+        for j in range(b):
+            print(i,j)
+
+
+
+print_items(4,5)
+
+
+
+# Output:
+
+# 0 0
+# 0 1
+# 0 2
+# 0 3
+# 0 4
+# 1 0
+# 1 1
+# 1 2
+# 1 3
+# 1 4
+# 2 0
+# 2 1
+# 2 2
+# 2 3
+# 2 4
+# 3 0
+# 3 1
+# 3 2
+# 3 3
+# 3 4
---
+++ Big O/O(a + B).py
@@ -0,0 +1,21 @@
+def print_items(a, b):
+    for i in range(a):
+        print(i)
+
+    for j in range(b):
+        print(j)
+
+
+(print_items(4, 5))
+
+
+# Output
+# 0
+# 1
+# 2
+# 3
+# 0
+# 1
+# 2
+# 3
+# 4
