94bc409bb5743f624e4b844bf0bfea9a77c931ef
Sumeet Deshpande <sumeet150621@gmail.com>
2023-07-15 18:04:04 +0530

    Add files via upload

---
+++ Linked Lists/LL-PopFirst.py
@@ -0,0 +1,66 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self,value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            temp = self.head
+            self.head = new_node
+            self.head.next = temp
+            self.length += 1
+
+    def PopFirst(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        self.head = self.head.next
+        temp.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.tail = None
+        return temp.value
+
+
+ll = LinkedList(2)
+ll.append(3)
+ll.append(4)
+ll.append(5)
+
+ll.print_list()
+
+print()
+print('After Prepend')
+ll.prepend(69)
+ll.print_list()
+
+print('Popped item', ll.PopFirst())
+ll.print_list()
