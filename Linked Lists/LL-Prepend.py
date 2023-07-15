c9e2080c0220446f43f80382543c004cbfc15fe0
Sumeet Deshpande <sumeet150621@gmail.com>
2023-07-15 10:02:04 +0530

    Add files via upload

---
+++ Linked Lists/LL-Prepend.py
@@ -0,0 +1,52 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+
+    def print_LL(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+
+
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+            self.length += 1
+            return True
+
+ll = LinkedList(2)
+ll.append(3)
+ll.append(7)
+ll.append(9)
+ll.print_LL()
+
+ll.prepend(69)
+print()
+print('New List afetr prepend')
+ll.print_LL()
