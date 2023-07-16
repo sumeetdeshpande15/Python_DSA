17666612c3a1c6cf246aff3acf4a38ba7108952d
Sumeet Deshpande <sumeet150621@gmail.com>
2023-07-16 10:49:27 +0530

    Add files via upload

---
+++ Linked Lists/LL-Insert.py
@@ -0,0 +1,72 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+    
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    def get(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        temp = self.head
+        for _ in range(index):
+            temp = temp.next
+        return temp
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+            self.length += 1
+            return True
+
+    def insert(self, index, value):
+        if index < 0 or index >= self.length:
+            return None
+        if index == 0:
+            return self.prepend(value)
+        if index == self.length:
+            return self.append(value)
+        new_node = Node(value)
+        temp = self.get(index-1)
+        new_node.next = temp.next
+        temp.next = new_node
+        self.length += 1
+        return True
+        
+ll = LinkedList(2)
+ll.append(3)
+ll.append(5)
+ll.append(6)
+ll.print_list()
+
+print('After Insert')
+ll.insert(2,4)
+ll.print_list()
